4e6a5131370a8fc9eb497e6539d83dde6aad79ae
abhi2892sharma <157945248+abhi2892sharma@users.noreply.github.com>
2024-02-11 15:15:54 +0530

    Update Temp change.cpp

--- Temp change.cpp
+++ Temp change.cpp
@@ -13,24 +13,24 @@ int main(){
     std::cout<<"Is temperature in Celcius(c) or Fahrenheit(f)? \n";
     std::cin>>a;
     
-    switch (a)
-    { case 'c':
-    std::cout<<"What is temperature in Celcius? \n";
-    std::cin>>Temp;    
-    f = ((Temp*9/5) + 32);
-    std::cout<<"The temperature in Fahrenheit is =" << f;
-    break;
-    
-    case 'f':
-    std::cout<<"What is temperature in Fahrenheit? \n";
-    std::cin>>Temp;
-    c = ((Temp - 32) * 5/9);
-    std::cout<<"The temperature in Celcius is =" << c;
-    break;
-
-    default: 
-    std::cout<<"This is invalid resposne \n";
-
+    if(a=='C'|| a=='c')
+    {
+        std::cout<<"What is temperature in Celcius? \n";
+        std::cin>>Temp;    
+        f = ((Temp*9/5) + 32);
+        std::cout<<"The temperature in Fahrenheit is =" << f;
     }
+    
+    else if(a =='f'|| a=='F')
+    {
+        std::cout<<"What is temperature in Fahrenheit? \n";
+        std::cin>>Temp;
+        c = ((Temp - 32) * 5/9);
+        std::cout<<"The temperature in Celcius is =" << c;
+    }
+
+    else 
+        std::cout<<"This is invalid resposne \n";
+
 
 }
